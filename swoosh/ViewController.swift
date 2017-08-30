effb7c56828de51c4f8ecd20565f511caf567425
Ding Zhiyi <minerva@DingdeMacBook-Pro.local>
2017-08-30 13:59:35 +0900

    Initial Commit

---
+++ swoosh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swoosh
+//
+//  Created by Ding Zhiyi on 30/08/2017.
+//  Copyright © 2017 Ding Zhiyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
