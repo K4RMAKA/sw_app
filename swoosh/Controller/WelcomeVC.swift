26415b66db1f4377f9ec8ab9af7b50248346133d
Ding Zhiyi <minerva@DingdeMacBook-Pro.local>
2017-08-31 14:10:39 +0900

    MVC

--- swoosh/Controller/WelcomeVC.swift
+++ swoosh/Controller/WelcomeVC.swift
@@ -8,14 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    @IBAction func unwindFromSkillVC(fromBkBtn:UIStoryboardSegue){
+    @IBAction func unwindFromLeagueVC(fromBkBtn:UIStoryboardSegue){
         
     }
 
