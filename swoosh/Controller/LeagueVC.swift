00efa8d04847ed1a8412a0dd3a3869eed4fe9adb
Ding Zhiyi <minerva@DingdeMacBook-Pro.local>
2017-08-31 15:10:48 +0900

    league2skill

--- swoosh/Controller/LeagueVC.swift
+++ swoosh/Controller/LeagueVC.swift
@@ -10,12 +10,33 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    var player:Player!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        player = Player()
     }
-
+    
+    @IBOutlet weak var nextBtn: MyUIButton!
+    
+    @IBAction func mensPressed(_ sender: Any) {
+        changeStatusPressed(changedTo: "Mens")
+    }
+    
+    @IBAction func womensPressed(_ sender: Any) {
+        changeStatusPressed(changedTo: "Womens")
+    }
+    
+    @IBAction func coedPressed(_ sender: Any) {
+        changeStatusPressed(changedTo: "Co-ed")
+    }
+    
+    func changeStatusPressed(changedTo:String){
+        player.leagueStatus = changedTo
+        nextBtn.isEnabled = true
+    }
+    
     @IBAction func skillVCSegue(_ sender: Any) {
         performSegue(withIdentifier: "SkillVCSegue", sender: self)
     }
---
+++ swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  swoosh
+//
+//  Created by Ding Zhiyi on 31/08/2017.
+//  Copyright © 2017 Ding Zhiyi. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var leagueStatus:String?
+    var skillStatus:String?
+}
